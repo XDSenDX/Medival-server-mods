2e02d5198c33a895b76894ab010885c0ee82aac1
XDSenDX <74653260+XDSenDX@users.noreply.github.com>
2024-06-04 12:17:55 +0700

    Huge update

---
+++ kubejs/server_scripts/example.js
@@ -0,0 +1,12 @@
+BlockEvents.rightClicked('minecraft:enchanting_table',e => {
+    e.cancel()
+}),
+BlockEvents.rightClicked('minecraft:anvil',e => {
+    e.cancel()
+}),
+BlockEvents.rightClicked('minecraft:chipped_anvil',e => {
+    e.cancel()
+}),
+BlockEvents.rightClicked('minecraft:damaged_anvil',e => {
+    e.cancel()
+})
